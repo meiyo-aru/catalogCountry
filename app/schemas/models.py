1b56311a345275debd1b716ed269aa0144a88f40
blink992 <alleybossmangas@gmail.com>
2025-08-28 00:46:21 -0300

    Remove unnecessary attributes from pydantic model

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -4,13 +4,7 @@ from pydantic import BaseModel
 class rest_countries_model(BaseModel):
     name: dict
     population: int
-    independent: bool
     region: str
-    subregion: str
-    continents: list
-    languages: dict
-    capital: list | None = None
-    currencies: dict | None = None
     likes: int = 0
     dislikes: int = 0
     class Config:
